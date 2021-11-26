53ba285a9c458ff6df1e8556cb0e09743b7551c5
Joshua Trimm <trimmj@etsu.edu>
2021-11-26 11:48:40 -0500

    Menu Updates

--- DSProject4Driver/MenuHelper.cs
+++ DSProject4Driver/MenuHelper.cs
@@ -16,14 +16,23 @@ namespace DSProject1
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Windows;
+    using System.Windows.Forms;
     using UtilityNamespace;
+    using Menu = UtilityNamespace.Menu;
+    using MessageBox = System.Windows.Forms.MessageBox;
 
     /// <summary>
     /// <see cref="MenuHelper" /> add functionality to Dr. Bailes MenuDemo package.
     /// </summary>
     public static class MenuHelper
     {
-        public static Menu Menu; 
+        public static Menu Menu;
+
+        private static int NumberOfWindows;
+        private static int ServiceTime;
+        private static int HoursOfOperation;
+        private static int NumberOfRegistrants;
 
         /// <summary>
         /// The Setup is used to set define how the console will look and prompt the user with a welcome message. It will also define what the menu items will say.
@@ -65,7 +74,17 @@ namespace DSProject1
                 {
                     case 1: // Set the number of Registrants
                         Console.WriteLine("How many Registrants are expected to be served in a day?");
-                        var number = Console.ReadLine();
+                        var numbOfReg = Console.ReadLine();
+
+                        var parsed = Int32.TryParse(numbOfReg, out NumberOfRegistrants);
+
+                        if(!parsed)
+                        {
+                            MessageBox.Show($"{DateTime.Now}\n User Input Error", $"Please only use a numeric value", (MessageBoxButtons)MessageBoxButton.OK, MessageBoxIcon.Information);
+                           
+                        }
+
+
                         break;
 
                     case 2: // Set the number of hours of operation
@@ -73,14 +92,30 @@ namespace DSProject1
 
                         Console.WriteLine("How many hours will registration be open?");
 
-                        Console.ReadKey();
+                        var numbOfHours = Console.ReadLine();
+
+                        var parsedInt = Int32.TryParse(numbOfHours, out HoursOfOperation);
+
+                        if (!parsedInt)
+                        {
+                            MessageBox.Show($"{DateTime.Now}\n User Input Error", $"Please only use a numeric value", (MessageBoxButtons)MessageBoxButton.OK, MessageBoxIcon.Information);
+
+                        }
                         break;
 
                     case 3: // Set the number of windows
 
                         Console.WriteLine("How many registration lines are to be simulated?");
 
-                        Console.ReadKey();
+                        var numbOfWindows = Console.ReadLine();
+
+                        var parsedIntWin = Int32.TryParse(numbOfWindows, out NumberOfWindows);
+
+                        if (!parsedIntWin)
+                        {
+                            MessageBox.Show($"{DateTime.Now}\n User Input Error", $"Please only use a numeric value", (MessageBoxButtons)MessageBoxButton.OK, MessageBoxIcon.Information);
+
+                        }
 
                         break;
 
@@ -89,8 +124,20 @@ namespace DSProject1
 
                         Console.ReadKey();
 
+                        var checkoutDuration = Console.ReadLine();
+
+                        var parsedIntService = Int32.TryParse(checkoutDuration, out ServiceTime);
+
+                        if (!parsedIntService)
+                        {
+                            MessageBox.Show($"{DateTime.Now}\n User Input Error", $"Please only use a numeric value", (MessageBoxButtons)MessageBoxButton.OK, MessageBoxIcon.Information);
+
+                        }
+
                         break;
                     case 5: // Run the simulation
+                        if(!PropertiesReadyForCalculation())
+                            MessageBox.Show($"{DateTime.Now}\n Please enter values for items 1-4. Value must be greater than 0 (Zero).", $"Values Not Set", (MessageBoxButtons)MessageBoxButton.OK, MessageBoxIcon.Information);
                         Console.ReadKey();
                         break;
                     case 6: // exit the program
@@ -100,6 +147,13 @@ namespace DSProject1
                 choiceNumber = Menu.GetChoice();
             }  // end of while
 
+        }
+
+        private static bool PropertiesReadyForCalculation()
+        {
+            if (NumberOfWindows == 0 || NumberOfRegistrants == 0 || ServiceTime == 0 || HoursOfOperation == 0)
+                return false;
+            return true;
         }
 
 
