6caa37794218f3cfa95aa255a8ef76d3bd4b878c
simmonsil <simmonsi@etsu.edu>
2021-11-26 14:12:11 -0500

    Updated IsEmpty and T Peek

--- PriorityQueue/PriorityQueue.cs
+++ PriorityQueue/PriorityQueue.cs
@@ -29,12 +29,19 @@ namespace PriorityQueue
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return Count == 0;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if(!IsEmpty())
+            {
+                return top.Item;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot obtain top of empty priority queue.");
+            }
         }
     }
 }
